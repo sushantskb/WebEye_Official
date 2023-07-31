b92c322d4c1fc71e73eb08e1e5ffdb795fb87a7a
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-08-01 01:13:47 +0530

    fixed the navbar problem

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,129 +9,129 @@ const Home = () => {
   const parallax = useRef(null);
 
   return (
-    <div style={{ width: '100%', height: '100%', background: '#253237' }}>
-      {/* The Parallax component wraps the layers */}
-      <Parallax ref={parallax} pages={3}>
-        {/* Layer 1 */}
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
+      <div style={{ width: '100%', height: '100%', background: '#253237' }} className='home'>
+        {/* The Parallax component wraps the layers */}
+        <Parallax ref={parallax} pages={3}>
+          {/* Layer 1 */}
+          <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
 
-        {/* Layer 2 */}
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+          {/* Layer 2 */}
+          <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
 
-        {/* Background Layer */}
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={3}
-          style={{
-            backgroundImage: url('stars', true),
-            backgroundSize: 'cover',
-          }}
-        />
+          {/* Background Layer */}
+          <ParallaxLayer
+            offset={0}
+            speed={0}
+            factor={3}
+            style={{
+              backgroundImage: url('stars', true),
+              backgroundSize: 'cover',
+            }}
+          />
 
-        {/* Satellite Layer */}
-        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} alt="Satellite" />
-        </ParallaxLayer>
+          {/* Satellite Layer */}
+          <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+            <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} alt="Satellite" />
+          </ParallaxLayer>
 
-        {/* Cloud Layers */}
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} alt="Cloud" />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} alt="Cloud" />
-        </ParallaxLayer>
+          {/* Cloud Layers */}
+          <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} alt="Cloud" />
+            <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} alt="Cloud" />
+          </ParallaxLayer>
 
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} alt="Cloud" />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} alt="Cloud" />
-        </ParallaxLayer>
+          <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} alt="Cloud" />
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} alt="Cloud" />
+          </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} alt="Cloud" />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} alt="Cloud" />
-        </ParallaxLayer>
+          <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} alt="Cloud" />
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} alt="Cloud" />
+          </ParallaxLayer>
 
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} alt="Cloud" />
-          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} alt="Cloud" />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} alt="Cloud" />
-        </ParallaxLayer>
+          <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} alt="Cloud" />
+            <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} alt="Cloud" />
+            <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} alt="Cloud" />
+          </ParallaxLayer>
 
-        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} alt="Cloud" />
-          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} alt="Cloud" />
-        </ParallaxLayer>
+          <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+            <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} alt="Cloud" />
+            <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} alt="Cloud" />
+          </ParallaxLayer>
 
-        {/* Earth Layer */}
-        <ParallaxLayer
-          offset={2.5}
-          speed={-0.4}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            pointerEvents: 'none',
-          }}
-        >
-          <img src={url('earth')} style={{ width: '60%' }} alt="Earth" />
-        </ParallaxLayer>
+          {/* Earth Layer */}
+          <ParallaxLayer
+            offset={2.5}
+            speed={-0.4}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              pointerEvents: 'none',
+            }}
+          >
+            <img src={url('earth')} style={{ width: '60%' }} alt="Earth" />
+          </ParallaxLayer>
 
-        {/* Clients Layer */}
-        <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: '80%',
-            backgroundPosition: 'center',
-            backgroundImage: url('clients', true),
-          }}
-        />
+          {/* Clients Layer */}
+          <ParallaxLayer
+            offset={2}
+            speed={-0.3}
+            style={{
+              backgroundSize: '80%',
+              backgroundPosition: 'center',
+              backgroundImage: url('clients', true),
+            }}
+          />
 
-        {/* Server, Bash, and Clients Main Layers */}
-        <ParallaxLayer
-          offset={0}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(1)}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          {/* <h1>Server</h1> */}
-          <img src={url('server')} style={{ width: '20%' }} alt="Server" />
-        </ParallaxLayer>
+          {/* Server, Bash, and Clients Main Layers */}
+          <ParallaxLayer
+            offset={0}
+            speed={0.1}
+            onClick={() => parallax.current.scrollTo(1)}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {/* <h1>Server</h1> */}
+            <img src={url('server')} style={{ width: '20%' }} alt="Server" />
+          </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={1}
-          speed={0.1}
-          onClick={() => parallax.current.scrollTo(2)}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          {/* <h1>Bash</h1> */}
-          <img src={url('bash')} style={{ width: '40%' }} alt="Bash" />
-        </ParallaxLayer>
+          <ParallaxLayer
+            offset={1}
+            speed={0.1}
+            onClick={() => parallax.current.scrollTo(2)}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {/* <h1>Bash</h1> */}
+            <img src={url('bash')} style={{ width: '40%' }} alt="Bash" />
+          </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={2}
-          speed={-0}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-          onClick={() => parallax.current.scrollTo(0)}
-        >
-          {/* <h1 className='client-text1'>Welcome</h1>
+          <ParallaxLayer
+            offset={2}
+            speed={-0}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onClick={() => parallax.current.scrollTo(0)}
+          >
+            {/* <h1 className='client-text1'>Welcome</h1>
           <h1 className='client-text2'>To</h1>
           <h1 className='client-text3'>Web</h1> */}
-          <img src={url('clients-main')} style={{ width: '40%' }} alt="Clients Main" />
-        </ParallaxLayer>
-      </Parallax>
-    </div>
+            <img src={url('clients-main')} style={{ width: '40%' }} alt="Clients Main" />
+          </ParallaxLayer>
+        </Parallax>
+      </div>
   );
 };
 
